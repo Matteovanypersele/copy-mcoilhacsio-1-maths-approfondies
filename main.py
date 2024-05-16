baca681e9b81d96b88483c9ed7e60260cfa6a088
Mireille COILHAC <mireille.coilhac@ac-creteil.fr>
2024-05-16 19:53:13 +0000

    MAJ

---
+++ main.py
@@ -0,0 +1,8 @@
+from pyodide_mkdocs_theme.pyodide_macros import PyodideMacrosPlugin, TestsToken
+
+def define_env(env:PyodideMacrosPlugin):
+
+    custom = {
+                "tests": TestsToken("\n# Tests"),
+                }
+    env.lang.overload(custom)
