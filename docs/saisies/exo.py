c71da32548914fd830f4430679a11282125dd83c
Mireille COILHAC <mireille.coilhac@ac-creteil.fr>
2024-05-18 07:08:37 +0000

    test saisie admonitions

---
+++ docs/saisies/exo.py
@@ -0,0 +1,32 @@
+# --------- PYODIDE:env --------- #
+
+def devine(reponse = None):
+    debut = NB_MIN
+    fin = NB_MAX
+    reussi = False
+    while (fin-debut >= 0 or erreur or reponse == None) and not reussi:
+        reussi = False
+        erreur = False
+        milieu = (fin + debut)//2
+        reponse = input(f"Si le nombre est strictement plus grand que {milieu} appuyer sur +, s'il est strictement plus petit appuyer sur - ,si c'est le bon appuyer sur =")
+        if reponse == '+':
+            debut=milieu + 1
+        elif reponse == '-':
+            fin = milieu - 1
+        elif reponse == '=':
+            print("Le nombre que vous avez choisi est : ", milieu)
+            reussi = True
+        else :
+            print("Erreur de saisie. Il faut saisir + ou - ou =")
+            erreur = True
+
+# --------- PYODIDE:code --------- #
+
+"""Vous jouez contre l'ordinateur. Le code de la fonction devine est caché.
+Vous devez penser à un nombre compris entre NB_MIN et NB_MAX.
+L'ordinateur va tenter de le deviner en vous posant des questions.
+Il faut juste cliquer pour exécuter."""
+
+NB_MIN = 1
+NB_MAX = 100
+devine()
---
+++ docs/saisies/intro_dicho.md
@@ -0,0 +1,12 @@
+??+ question "Je joue contre l'ordinateur"
+
+    Question 1.   
+
+    Exécuter le programme du jeu du nombre mystère.
+    Faire quelques parties, expliquer la stratégie de l'ordinateur pour trouver le nombre mystère.
+
+    {{ IDE('exo') }} 
+
+    ??? success "Solution"
+
+        A chaque fois, il se place au milieu.
