9eeb49402b23f29e67ad77e23cef168e80d75415
Mireille COILHAC <mireille.coilhac@ac-creteil.fr>
2024-06-05 14:01:07 +0000

    on vide main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from pyodide_mkdocs_theme.pyodide_macros import PyodideMacrosPlugin, TestsToken
+# Compléter ici vos macros personnelles
+# Explications ici : https://frederic-zinelli.gitlab.io/pyodide-mkdocs-theme/custom/extra_macros/
 
-def define_env(env:PyodideMacrosPlugin):
+#from pyodide_mkdocs_theme.pyodide_macros import PyodideMacrosPlugin
 
-    custom = {
-                "tests": TestsToken("\n# Tests"),
-                }
-                
+#def define_env(env:PyodideMacrosPlugin):
+
+    #@env.macro
+    #def macro1(...) -> str :
+        #return ...
